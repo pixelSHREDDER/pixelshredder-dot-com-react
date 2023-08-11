541062321d3520fc9e77a930995e51d7e72db291
Mike DeVine <pixelshredder@gmail.com>
2023-08-10 17:45:09 -0700

    Offline and next config fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
-    },
-    reactStrictMode: true,
-}
-
 const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
 module.exports = withPWA({
-  nextConfig
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  reactStrictMode: true,
 })  
---
+++ pages/_offline.tsx
@@ -0,0 +1,18 @@
+import styles from '/styles/utils.module.css';
+import Layout, { LayoutProps } from '../components/layout';
+import GlobalHead from '../components/head';
+
+export default function Offline(props: LayoutProps) {
+  return (
+    <Layout {...props}>
+      <GlobalHead meta={{
+        title: 'Mike DeVine | Developer/Designer/Creative/Politico',
+        description: 'Website of Mike DeVine, aka pixelSHREDDER - Web/Game Developer, Web/Graphic/UI/UX/Game Designer, Creative Professional, Politico, Writer, Funnyperson.'
+      }} />
+      <article className={styles.homecontent}>
+        <h1 className={styles.title}>Offline!</h1>
+        <p className={styles.description}>Please reload and try again.</p>
+      </article>
+    </Layout>
+  )
+}
