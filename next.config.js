ed94bd549189c939a5ed23260ac2361d55d821eb
Mike DeVine <pixelshredder@gmail.com>
2023-08-22 20:28:25 -0700

    Domain redirects  / path rewrites fix

--- next.config.js
+++ next.config.js
@@ -10,4 +10,20 @@ module.exports = withPWA({
     defaultLocale: "en",
   },
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/mikearcade/:path*',
+        destination: 'https://pixel-shredder.com/mikearcade/:path*',
+      },
+      {
+        source: '/mikebox/:path*',
+        destination: 'https://pixel-shredder.com/mikebox/:path*',
+      },
+      {
+        source: '/port/:path*',
+        destination: 'https://pixel-shredder.com/port/:path*',
+      },
+    ]
+  },
 })  
